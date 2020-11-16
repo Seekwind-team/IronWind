b3c851f51edfb4bdabf2ddb29d31f6577e69d709
Glasgesicht <BernhardtAlexanderScheibner@Outlook.com>
2020-11-16 15:54:54 +0100

    [SEP-50]
    Added Model fpr Job Offers

---
+++ joboffer/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from user.models import User
+from django.utils import timezone
+
+
+class JobOffer:
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    hashtags = models.TextField()
+    jobCats = models.TextField()
+    filled = models.TextField()
+    isDeleted = models.TextField()
+
+    JOBTYPE_CHOICES = [
+        'Vollzeit',
+        'Teilzeit',
+        'Ausbildung',
+        'Praktikum',
+        'Volunteer'
+    ]
+
+    jobType = models.CharField(
+        max_length=100,
+        choices=JOBTYPE_CHOICES,
+        default='Vollzeit'
+    )
+
+    jobTitle = models.CharField(
+        max_length=255,
+        blank=False
+    )
+
+    # Hier sollte sich überlegt werden, wie mit Geo-Locations umgegangen wird
+    location = models.CharField(
+        max_length=100,
+    )
+
+    description = models.TextField(
+        max_length=4000
+    )
+
+    highlights = models.TextField(
+        max_length=1000
+    )
+    must_have = models.TextField(
+        max_length=1000,
+    )
+    public_email = models.EmailField(
+
+    )
+    company_logo = models.ImageField()
+    created_at = models.DateTimeField(
+        editable=False
+    )
+    last_modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = timezone.now()
+        self.last_modified = timezone.now()
+        return super(User, self).save(*args, **kwargs)
+
+
+# used to store Images for Joboffers
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    model = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    default = models.BooleanField(default=False)
+    width = models.FloatField(default=100)
+    length = models.FloatField(default=100)
