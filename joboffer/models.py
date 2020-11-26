ff22fb033b20e2c45bc75f3facf668badcaf7eea
Glasgesicht <BernhardtAlexanderScheibner@Outlook.com>
2020-11-26 14:58:40 +0100

    Added Help fields to Models

--- joboffer/models.py
+++ joboffer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from user.models import Authentication
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class JobOffer(models.Model):
@@ -10,8 +11,9 @@ class JobOffer(models.Model):
     )
     # hashtags = models.TextField()
     # job_cats = models.TextField()
-    filled = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=False)
+    filled = models.BooleanField(default=False,
+                                 help_text=_('Definmiert, ob ein Jobangebot besetzt ist'))
+    is_deleted = models.BooleanField(default=False, help_text=_('definiert, ob ein Jobangebot "gelöscht" ist'))
 
     JOBTYPE_CHOICES = [
         ('Vollzeit', 'Vollzeit'),
@@ -24,44 +26,53 @@ class JobOffer(models.Model):
     job_type = models.CharField(
         max_length=100,
         choices=JOBTYPE_CHOICES,
-        default='Vollzeit'
+        default='Vollzeit',
+        help_text=_('Typ des Jobangebotes, z.B. "Vollzeit"')
     )
 
     job_title = models.CharField(
         max_length=255,
-        null=True
+        null=True,
+        help_text=_('Titel (Name) des Jobangebots')
     )
 
     # Hier sollte sich überlegt werden, wie mit Geo-Locations umgegangen wird
     location = models.CharField(
         max_length=100,
-        null=True
+        null=True,
+        help_text=_('Ort des Jobangebots')
     )
 
     description = models.TextField(
         max_length=4000,
-        null=True
+        null=True,
+        help_text=_('Beschreibung des Jobangebots')
     )
 
     highlights = models.TextField(
         max_length=1000,
-        null=True
+        null=True,
+        help_text=_('Highlights des Jobangebots')
     )
     must_have = models.TextField(
         max_length=1000,
-        null=True
+        null=True,
+        help_text=_('Must Haves des Jobangebots (z.B. Führerschein)')
     )
 
     public_email = models.EmailField(
-        null=True
+        null=True,
+        help_text=_('Öffentlich sichtbare EMail-Adresse')
     )
 
     #  company_logo = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(
-        default=timezone.now
+        default=timezone.now,
+        help_text=_('Erstellungs Datum und Zeit')
     )
     last_modified = models.DateTimeField(
-        default=timezone.now
+        default=timezone.now,
+        help_text=_('Zeitpunkt der letzten Änderung')
     )
 
 
