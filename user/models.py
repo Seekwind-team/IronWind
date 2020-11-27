b38f4b47c33c36bc2fe101a2f81a16f8124cf048
gabrielE <51362934+1826914@users.noreply.github.com>
2020-11-27 16:38:06 +0100

    prevent overlapping namespaces
    changed source to src

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 from django.utils.translation import gettext_lazy as _
 
-from source import settings
+from src import settings
 
 
 class UserManager(BaseUserManager):
