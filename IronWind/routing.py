cf3b5268076cc892c4cff13d056a55b71c7bb8bd
Glasgesicht <BernhardtAlexanderScheibner@Outlook.com>
2020-12-11 14:41:02 +0100

    Update routing.py

--- IronWind/routing.py
+++ IronWind/routing.py
@@ -1,6 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import re_path
+from django.urls import path
 
 
 from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
@@ -9,6 +9,6 @@ application = ProtocolTypeRouter({
 
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            re_path('', GraphqlSubscriptionConsumer)
+            path('', GraphqlSubscriptionConsumer)
     ])),
 })
