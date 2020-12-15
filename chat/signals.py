dbcecbe077c952bbb6848d6e091f55480514e6f5
Gabriel E <51362934+1826914@users.noreply.github.com>
2020-12-15 17:16:24 +0100

    Update signals.py

--- chat/signals.py
+++ chat/signals.py
@@ -1,5 +1,5 @@
 # signals.py
-from django.db.models.signals import post_save , post_delete
+from django.db.models.signals import post_save, post_delete
 from graphene_subscriptions.signals import post_save_subscription, post_delete_subscription
 
 from chat.models import Message
