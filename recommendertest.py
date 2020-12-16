f73a872067535e4325e5668b9f866e67af676271
blackbird <blackbird97@gmx.net>
2020-12-16 14:34:55 +0100

    Create recommendertest.py

---
+++ recommendertest.py
@@ -0,0 +1,8 @@
+import recommenderClass as rc
+myRec = rc.Recommender()
+recommendations = myRec.recommend("5fcf78c21be24100126e19bf")
+print(recommendations)
+recommendations = myRec.recommend("5fcbb5221be24100126e18c0")
+print(recommendations)
+recommendations = myRec.recommend("5fcb8d0bace2770012009652")
+print(recommendations)
