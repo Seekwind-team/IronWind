c5fcea095f21f574989cf4491767fa1506124637
Gabriel E <1826914@stud.hs-mannheim.de>
2021-01-06 18:47:49 +0100

    created some setups

--- joboffer/tests.py
+++ joboffer/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
 
-# Create your tests here.
+from joboffer.models import JobOffer, Authentication
+
+class JobOfferTestCase(TestCase):
+    def setUp(self):
+        # owner Object
+        owner = Authentication.objects.create_user(
+            email = "muster@maxmann.aha",
+            password = "123",
+            # im going to create 'create user' method in user tests to use here    
+        )
+
+        owner.is_company = True
+
+        # maximal filled JobOffer
+        max_joboffer = JobOffer.objects.create(
+            owner = owner,
+            #hashtags = TBD,
+            filled = False,
+            is_deleted = False,
+            job_type = 'Vollzeit',
+            job_title = 'max Jobangebot',
+            location = 'Heidenheim',
+            description = 'das ist 1 guter Job',
+            highlights = 'ganz viele',
+            must_have = 'alle bitte',
+            nice_have = 'öalsidhf',
+            public_email = owner.email,
+            pay_per_year = '777,1230,13',
+            city = 'Engelsberg',
+            start_date = '2020-11-23',
+            trade = 'Arbeiter',
+            # created_at and last_modified are set to default (now),
+        )
+
+        # minimal filled JobOffer
+        min_joboffer = JobOffer.objects.create(
+            owner = owner,
+            job_title = 'min Jobangebot',
+            # created_at and last_modified are set to default (now),
+        )
+
+        max_joboffer.save()
+        min_joboffer.save()
+
+    def test_job_offer_to_string(self):
+        max_joboffer = JobOffer.objects.get(job_title='max Jobangebot')
+        min_joboffer = JobOffer.objects.get(job_title='min Jobangebot')
+
+        # .._joboffer.pk might cause errors, if so try .._joboffer.id
+        max_str = 'Joboffer ({}) "{}"'.format(max_joboffer.pk, max_joboffer.job_title)
+        min_str = 'Joboffer ({}) "{}"'.format(min_joboffer.pk, min_joboffer.job_title)
+
+        self.assertEqual(max_str, max_joboffer.__str__())
+        self.assertEqual(min_str, min_joboffer.__str__())
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import UserData, CompanyData, Authentication
+from user.schema import CreateUser
+
+class DemoUser():
+    EMAIL = 'demo@must.er'
+    PW = '1234'
+
+    def __init__():
+        user = CreateUser(user_mutation())
+
+    def user_mutation(self):
+        mutation = """
+            muatation{
+                
+            }
+        """
+    # getter functions
+    def mail(self): 
+        return EMAIL
+
+    def pw(self): 
+        return PW
+
+    def acc(self): 
+        return 
+
+class UserTestCase(TestCase):
+    def setUp(self):
+
+    def 
