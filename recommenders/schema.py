7b9ab42559da45fde255ef669202f3d7cf79b691
Gabriel E <1826914@stud.hs-mannheim.de>
2021-01-08 15:36:38 +0100

    changed schema

--- recommenders/schema.py
+++ recommenders/schema.py
@@ -4,22 +4,17 @@ import user.schema
 
 from graphql_jwt.decorators import login_required
 
-from recommenders.recommenderClass import Recommender
+from recommenders.recommender import Recommender
 from joboffer.models import JobOffer
 from joboffer.schema import JobOfferType
 
-RCMDR = Recommender.new()
-
 class Query(graphene.ObjectType):
+    RCMDR = Recommender()
     my_recommendations = graphene.List(JobOfferType)
 
     # needs testing
     @login_required
     def resolve_my_recommendations(self, info):
-        RCMDR.preprocessing()
-        RCMDR.createBow()
-        RCMDR.createSimilarityMatrix()
-        
         user_id = info.context.user.id
 
         return RCMDR.recommend(Recommender, user_id)
