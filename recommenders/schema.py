08b0f2c87f839b07e680eeacfab37eefac1e2696
Gabriel E <51362934+1826914@users.noreply.github.com>
2021-01-08 16:16:23 +0100

    Update schema.py
    working state

--- recommenders/schema.py
+++ recommenders/schema.py
@@ -11,12 +11,9 @@ from joboffer.schema import JobOfferType
 class Query(graphene.ObjectType):
     my_recommendations = graphene.List(JobOfferType)
     
-    def __init__():
-        self.RCMDR = Recommender()
-    
     # needs testing
     @login_required
     def resolve_my_recommendations(self, info):
         user_id = info.context.user.id
-
-        return self.RCMDR.recommend(Recommender, user_id)
+        r = Recommender()
+        return r.recommend(user_id)
