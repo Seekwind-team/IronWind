ee575814a50144fabf631f53ad44ece1f7246756
Benjamin Mayer <1823720@stud.hs-mannheim.de>
2021-01-12 12:19:29 +0100

    renaming for better pytest integration

--- tests/int-test_all.py
+++ tests/int-test_all.py
@@ -1,23 +1,23 @@
-from importlib import __import__
-from glob import glob
-import os
-import re
-from pprint import pprint as print
+# from importlib import __import__
+# from glob import glob
+# import os
+# import re
+# from pprint import pprint as print
 
-def create_test(module_name):
-    def func():
-        __import__("int-test_" + module_name).test()
-    func.__name__ = "test" + module_name
-    return func
+# def create_test(module_name):
+#     def func():
+#         __import__("int-test_" + module_name).test()
+#     func.__name__ = "test" + module_name
+#     return func
 
-testfiles = glob(os.path.dirname(os.path.abspath(__file__)) + r"\int-test_*")
-thisfile = glob(os.path.dirname(os.path.abspath(__file__)) + r"\int-test_all.py")[0]
-testfiles.remove(thisfile)
+# testfiles = glob(os.path.dirname(os.path.abspath(__file__)) + r"\int-test_*")
+# thisfile = glob(os.path.dirname(os.path.abspath(__file__)) + r"\int-test_all.py")[0]
+# testfiles.remove(thisfile)
 
-modules = []
+# modules = []
 
-for file in testfiles:
-	modules.append(re.search(r"int-test_.*.py", file).group(0)[9:-3])
+# for file in testfiles:
+# 	modules.append(re.search(r"int-test_.*.py", file).group(0)[9:-3])
 
-for module in modules:
-    globals()['test_' + module] = create_test(module)
+# for module in modules:
+#     globals()['test_' + module] = create_test(module)
--- tests/testhub.py
+++ tests/testhub.py
@@ -1,39 +1,39 @@
-import importlib
-import sys
-import os
-import re
-from glob import glob
-from helper import Logger
+# import importlib
+# import sys
+# import os
+# import re
+# from glob import glob
+# from helper import Logger
 
 
 
-cmd_args = sys.argv
+# cmd_args = sys.argv
 
-tests = []
-logfile_name = None
+# tests = []
+# logfile_name = None
 
-if len(cmd_args) > 1:
+# if len(cmd_args) > 1:
 
-	if '-l' in cmd_args:
-		index = cmd_args.index('-l')
-		cmd_args.pop(index)
-		logfile_name = os.path.dirname(os.path.abspath(__file__)) +  "\\logs\\" + sys.argv[-1] + ".txt"
-		cmd_args.pop(-1)
+# 	if '-l' in cmd_args:
+# 		index = cmd_args.index('-l')
+# 		cmd_args.pop(index)
+# 		logfile_name = os.path.dirname(os.path.abspath(__file__)) +  "\\logs\\" + sys.argv[-1] + ".txt"
+# 		cmd_args.pop(-1)
 
-	tmp_tests = cmd_args[1:]
-	for test in tmp_tests:
-		tests.append("int-test_"  + test)
-
-
-if not tests:
-	testfiles = glob(os.path.dirname(os.path.abspath(__file__)) + "\\int-test_*")
-	for file in testfiles:
-		tests.append(re.search("int-test_.*\.py", file).group(0)[:-3])
+# 	tmp_tests = cmd_args[1:]
+# 	for test in tmp_tests:
+# 		tests.append("int-test_"  + test)
 
 
-logger = Logger(logfile_name)
+# if not tests:
+# 	testfiles = glob(os.path.dirname(os.path.abspath(__file__)) + "\\int-test_*")
+# 	for file in testfiles:
+# 		tests.append(re.search("int-test_.*\.py", file).group(0)[:-3])
 
-def test():
-	for test in tests:
-		testfile = importlib.__import__(test)
-		testfile.test()
+
+# logger = Logger(logfile_name)
+
+# def test():
+# 	for test in tests:
+# 		testfile = importlib.__import__(test)
+# 		testfile.test()
