449205386f7d6302174920a3246425fa70ff3c6e
Glasgesicht <BernhardtAlexanderScheibner@Outlook.com>
2021-01-12 11:13:40 +0100

    Update schema.py

--- joboffer/schema.py
+++ joboffer/schema.py
@@ -160,7 +160,7 @@ class AlterJobOffer(graphene.Mutation):
         pay_per_year = graphene.List(graphene.String, description="Zu erwartendes Gehalt und des einzelnen Ausbildungsjahren")
         pay_per_hour = graphene.Int(description="Stundenlohn")
         city = graphene.String(description="Ort des Jobangebots")
-        start_date = graphene.String(description="Datum des ersten Arbeitstages")
+        start_date = graphene.Date(description="Datum des ersten Arbeitstages")
         trade = graphene.String(description="Jobkategorie")
 
     @user_passes_test(lambda user: user.is_company)
