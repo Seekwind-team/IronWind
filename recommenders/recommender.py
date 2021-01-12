2fe0174db954ff20efb518da05e2b840cfd90840
blackbird <blackbird97@gmx.net>
2021-01-12 16:52:46 +0100

    Update recommender.py

--- recommenders/recommender.py
+++ recommenders/recommender.py
@@ -6,16 +6,19 @@ from joboffer.models import *
 
 class Recommender:
 
-    df = None
-    corrMatrix = None
-    id2index = {}
-    index2id = {}
-    cosine_sim = None
+    df = None #<-- DataFrame for all matches with userid, jobid, like, (job-) description, location and title
+    corrMatrix = None #<-- Matrix of all jobs and their similarity based on user votes 
+    id2index = {} ### 2 dictionaries to look up real job ids and their indexes in a matrix
+    index2id = {} ###
+    cosine_sim = None # Matrix of all jobs and their similarity based on NLP
 
     def __init__(self):
         self.update()
 
+    #update the matching data and the similarity matrices
     def update(self):
+
+        # filling the dataframe with data
         self.df = pd.DataFrame()       
         swipes = Swipe.objects.all()
         jobidlist = []
@@ -45,21 +48,21 @@ class Recommender:
         allUserRatings = self.df.pivot_table(index=['user_id'],columns=['job_id'],values='like')
         self.corrMatrix = allUserRatings.corr(method="pearson")
 
+    #clean data of relevant columns
     def preprocessing(self):
-        
         for index, row in self.df.iterrows():
-            columns = ["description"]
+            columns = ["description","jobtitle","location"]
             for col in columns:
                 #cleaning data
                 row[col] = str(row[col]).replace("­","")
 
+    #create a bag of words for each job to measure similarity of jobs based on features
     def createBow(self):
-        
-        #choose important columns
+        #choose important columns for bag of words
         columns = ["description","jobtitle","location"]
         wordlist = []
         for index, row in self.df.iterrows():
-            #create Dictionaries for matrix
+            #create Dictionaries for matrices
             self.id2index[row["job_id"]] = index
             self.index2id[index] = row["job_id"]
             words = ""
@@ -69,6 +72,7 @@ class Recommender:
             wordlist.append(words)
         self.df["bow"] = wordlist
     
+    #create a similarity matrix based on job features
     def createSimilarityMatrix(self):
         
         # create vectorizer for bag of words
@@ -77,6 +81,7 @@ class Recommender:
         cm = count.fit_transform(self.df["bow"])
         self.cosine_sim = cosine_similarity(cm)
 
+    #get similar jobs to liked jobs
     def get_similar_jobs(self, jobid):
         recommended_jobs = []
         #find index of given job in matrix
@@ -84,16 +89,17 @@ class Recommender:
         score_series = pd.Series(self.cosine_sim[idx]).sort_values(ascending = False)
         top_indices = list(score_series.iloc[1:2].index)
         for i in top_indices:
-            topjobid = self.index2id[i] ## <- Tested: returns correct _id
-            #print(topjobid)
+            topjobid = self.index2id[i]
             recommended_jobs.append(topjobid)
         return recommended_jobs
 
+    #get jobs rated similarly to already rated jobs
     def evaluate(self,jobID,rating):
         similar_ratings = self.corrMatrix[jobID]*(rating)
         similar_ratings = similar_ratings.sort_values(ascending=False)
         return similar_ratings
     
+    #get all jobs rated by one user
     def get_rated_jobs(self, user_id):
         rated_jobs = self.df[self.df["user_id"] == user_id]
         ratings = []
@@ -102,6 +108,8 @@ class Recommender:
             ratings.append(tmp)
         return ratings
 
+    #get a list of top10 jobs that might be interesting for the user
+    #based on jobs similar to those he already liked and jobs that other users liked
     def recommend(self, user_id):
         recommended_jobs = pd.DataFrame()
         userRatings = self.get_rated_jobs(user_id)
@@ -111,7 +119,6 @@ class Recommender:
             result = list(fewrated.iloc[:10].index)
         #User already rated enough jobs
         else:
-            #print(userRatings)
             for jobid, rating in userRatings:
                 recommended_jobs = recommended_jobs.append(self.evaluate(jobid, rating), ignore_index = True)
 
@@ -124,6 +131,6 @@ class Recommender:
             if rating == 1:
                 result += self.get_similar_jobs(jobid)
 
-        result = result[:11]
+        result = result[:10]
         return result
         #return recommended_jobs
