e237c2f708e8e24735dba0935e1711f5e623e28f
blackbird <blackbird97@gmx.net>
2021-01-14 16:25:42 +0100

    Update recommender.py

--- recommenders/recommender.py
+++ recommenders/recommender.py
@@ -6,7 +6,8 @@ from joboffer.models import *
 
 class Recommender:
 
-    df = None #<-- DataFrame for all matches with userid, jobid, like, (job-) description, location and title
+    swipesdf = None #<-- DataFrame for all swipes with userid, jobid, like, 
+    jobsdf = None#<-- DataFrame for all jobs with (job-) description, location and title
     corrMatrix = None #<-- Matrix of all jobs and their similarity based on user votes 
     id2index = {} ### 2 dictionaries to look up real job ids and their indexes in a matrix
     index2id = {} ###
@@ -17,40 +18,60 @@ class Recommender:
 
     #update the matching data and the similarity matrices
     def update(self):
+        # filling Swipe-Dataframe and Job-Dataframe
+        self.getSwipesData()
+        self.getJobsData()
 
-        # filling the dataframe with data
-        self.df = pd.DataFrame()       
+        self.preprocessing()
+        self.createBow()
+        self.createSimilarityMatrix()
+        self.createCorrelationMatrix()
+
+    #collect data of all swipes and store them in a DataFrame "swipesdf"
+    def getSwipesData(self):
+        self.swipesdf = pd.DataFrame()
         swipes = Swipe.objects.all()
         jobidlist = []
         useridlist = []
         likelist = []
-        descriptionlist = []
-        locationlist = []
-        titlelist = []
-        
+
+        #iterate over all swipes and collect data in lists
         for s in swipes:
             jobidlist.append(s.job_offer.id)
             useridlist.append(s.candidate.id)
             likelist.append(s.liked)
-            descriptionlist.append(s.job_offer.description)
-            locationlist.append(s.job_offer.location)
-            titlelist.append(s.job_offer.job_title)
+        
+        #store data of lists in DataFrame
+        self.swipesdf["job_id"] = jobidlist
+        self.swipesdf["user_id"] = useridlist
+        self.swipesdf["like"] = likelist
 
-        self.df["job_id"] = jobidlist
-        self.df["user_id"] = useridlist
-        self.df["like"] = likelist
-        self.df["description"] = descriptionlist
-        self.df["location"] = locationlist
-        self.df["jobtitle"] = titlelist
-        self.preprocessing()
-        self.createBow()
-        self.createSimilarityMatrix()
-        allUserRatings = self.df.pivot_table(index=['user_id'],columns=['job_id'],values='like')
-        self.corrMatrix = allUserRatings.corr(method="pearson")
+    #collect data of all jobs and store them in a Dataframe "jobsdf"
+    def getJobsData(self):
+        self.jobsdf = pd.DataFrame()
+        jobs = JobOffer.objects.all()
+        jobidlist = []
+        descriptionlist = []
+        locationlist = []
+        titlelist = []
+
+        #iterate over all jobs and collect data in lists
+        for j in jobs:
+            jobidlist.append(j.id)
+            descriptionlist.append(j.description)
+            locationlist.append(j.location)
+            titlelist.append(j.job_title)
+
+        #store data of lists in DataFrame
+        self.jobsdf["job_id"] = jobidlist
+        self.jobsdf["description"] = descriptionlist
+        self.jobsdf["location"] = locationlist
+        self.jobsdf["jobtitle"] = titlelist
+
 
     #clean data of relevant columns
     def preprocessing(self):
-        for index, row in self.df.iterrows():
+        for index, row in self.jobsdf.iterrows():
             columns = ["description","jobtitle","location"]
             for col in columns:
                 #cleaning data
@@ -58,27 +79,30 @@ class Recommender:
 
     #create a bag of words for each job to measure similarity of jobs based on features
     def createBow(self):
-        #choose important columns for bag of words
+        #index for matrix that is created later
+        matrixidx = 0
+        #choose important columns to collect words from.
+        #all collected words will be stored in a "bag of words" for each jobs
         columns = ["description","jobtitle","location"]
         wordlist = []
-        for index, row in self.df.iterrows():
+        for index, row in self.jobsdf.iterrows():
             #create Dictionaries for matrices
-            self.id2index[row["job_id"]] = index
-            self.index2id[index] = row["job_id"]
+            self.id2index[row["job_id"]] = matrixidx #necessary because the indices of the similarity Matrix
+            self.index2id[matrixidx] = row["job_id"] #are integers (starting by 0) and not the jobids
+            matrixidx += 1
             words = ""
             for col in columns:
                 if row[col] is not None:
                     words += str(row[col])
             wordlist.append(words)
-        self.df["bow"] = wordlist
+        self.jobsdf["bow"] = wordlist 
     
     #create a similarity matrix based on job features
     def createSimilarityMatrix(self):
-        
         # create vectorizer for bag of words
         count = CountVectorizer()
         # create count matrix
-        cm = count.fit_transform(self.df["bow"])
+        cm = count.fit_transform(self.jobsdf["bow"])
         self.cosine_sim = cosine_similarity(cm)
 
     #get similar jobs to liked jobs
@@ -87,11 +111,16 @@ class Recommender:
         #find index of given job in matrix
         idx = self.id2index[jobid]
         score_series = pd.Series(self.cosine_sim[idx]).sort_values(ascending = False)
-        top_indices = list(score_series.iloc[1:2].index)
+        top_indices = list(score_series.iloc[1:3].index)
         for i in top_indices:
-            topjobid = self.index2id[i]
+            topjobid = self.index2id[i] #<-- convert matrix index to job_id
             recommended_jobs.append(topjobid)
         return recommended_jobs
+
+    #create a correlation matrix based on user votes    
+    def createCorrelationMatrix(self):
+        allUserRatings = self.swipesdf.pivot_table(index=['user_id'],columns=['job_id'],values='like')
+        self.corrMatrix = allUserRatings.corr(method="pearson")
 
     #get jobs rated similarly to already rated jobs
     def evaluate(self,jobID,rating):
@@ -101,7 +130,7 @@ class Recommender:
     
     #get all jobs rated by one user
     def get_rated_jobs(self, user_id):
-        rated_jobs = self.df[self.df["user_id"] == user_id]
+        rated_jobs = self.swipesdf[self.swipesdf["user_id"] == user_id]
         ratings = []
         for index, row in rated_jobs.iterrows():
             tmp = [row["job_id"],row["like"]]
@@ -111,11 +140,12 @@ class Recommender:
     #get a list of top10 jobs that might be interesting for the user
     #based on jobs similar to those he already liked and jobs that other users liked
     def recommend(self, user_id):
+        result = []
         recommended_jobs = pd.DataFrame()
         userRatings = self.get_rated_jobs(user_id)
         #User didnt rate enough jobs for identifying similar users
         if len(userRatings) < 10:
-            fewrated = self.df.groupby(by="job_id")["like"].count().sort_values(ascending = True)
+            fewrated = self.swipesdf.groupby(by="job_id")["like"].count().sort_values(ascending = True)
             result = list(fewrated.iloc[:10].index)
         #User already rated enough jobs
         else:
@@ -126,7 +156,7 @@ class Recommender:
             recommended_jobs = list(recommended_jobs.iloc[:10].index)
             result = recommended_jobs
         
-        #Remove jobs from list, that were already rated by User
+        #Add jobs to list, that are similar to jobs user already liked
         for jobid, rating in userRatings:
             if rating == 1:
                 result += self.get_similar_jobs(jobid)
