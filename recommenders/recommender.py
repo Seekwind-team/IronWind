55e94d749d39490da6ba963c9fa01cb6eada4a81
blackbird <blackbird97@gmx.net>
2021-01-15 15:11:24 +0100

    Update recommender.py

--- recommenders/recommender.py
+++ recommenders/recommender.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.utils import shuffle
 from joboffer.models import *
 
 class Recommender:
