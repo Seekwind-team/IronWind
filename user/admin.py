2a1d0d3090cfd635cc24c123265f70d18a8b6837
Glasgesicht <BernhardtAlexanderScheibner@Outlook.com>
2021-01-18 17:24:48 +0100

    bug fix

--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Authentication, UserData, CompanyData
+from .models import Authentication, UserData, CompanyData, Badges
+
 admin.site.register(Authentication)
 admin.site.register(CompanyData)
 admin.site.register(UserData)
+admin.site.register(Badges)
